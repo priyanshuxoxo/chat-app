546412f3d5128a47e050048a69647081ca507925
priyanshuxoxo <srivastavapriyanshu201@gmail.com>
2026-02-03 18:28:59 +0530

    added message routes and schema

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import connectDB from "./lib/db.js";
 dotenv.config();
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
